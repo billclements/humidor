0a1a979722a591679b6195cf98d0ab9d61da50da
billclements <william_clements1@student.uml.edu>
2013-03-14 19:10:42 -0400

    Added sample data to the DbHelper

--- Humidor/src/com/TDD/Humidor/DbHelper.java
+++ Humidor/src/com/TDD/Humidor/DbHelper.java
@@ -1,5 +1,6 @@
 package com.TDD.Humidor;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -42,6 +43,54 @@ public class DbHelper extends SQLiteOpenHelper {
 		//uses the String made earlier to create the db
 		db.execSQL(CREATE_DB);
 		
+		//Following code block will provide sample data
+		//It should be deleted or commented out upon completion		
+		SQLiteDatabase d = this.getWritableDatabase();
+
+	    ContentValues values = new ContentValues();
+	    
+	    values.put(BRAND, "Perdomo");
+	    values.put(TYPE, "Lot 23");
+	    values.put(WRAPPER, "Connecticut");
+	    values.put(VITOLA, "Robusto");
+	    values.put(QUANTITY, 5);
+	    d.insert(DBTABLE, null, values);
+	    
+	    values.put(BRAND, "Alec Bradley");
+	    values.put(TYPE, "Tempus");
+	    values.put(WRAPPER, "Natural");
+	    values.put(VITOLA, "Toro");
+	    values.put(QUANTITY, 5);
+	    d.insert(DBTABLE, null, values);
+	    
+	    values.put(BRAND, "5 Vegas");
+	    values.put(TYPE, "Miami");
+	    values.put(WRAPPER, "Habano");
+	    values.put(VITOLA, "Toro");
+	    values.put(QUANTITY, 10);
+	    d.insert(DBTABLE, null, values);
+		
+	    values.put(BRAND, "5 Vegas");
+	    values.put(TYPE, "Triple-A");
+	    values.put(WRAPPER, "Maduro");
+	    values.put(VITOLA, "Lancero");
+	    values.put(QUANTITY, 5);
+	    d.insert(DBTABLE, null, values);
+	    
+	    values.put(BRAND, "Perdomo");
+	    values.put(TYPE, "Habano");
+	    values.put(WRAPPER, "Connecticut");
+	    values.put(VITOLA, "Toro");
+	    values.put(QUANTITY, 15);
+	    d.insert(DBTABLE, null, values);
+	    
+	    values.put(BRAND, "Man O' War");
+	    values.put(TYPE, "Ruination");
+	    values.put(WRAPPER, "Natural");
+	    values.put(VITOLA, "Robusto");
+	    values.put(QUANTITY, 12);
+	    d.insert(DBTABLE, null, values);
+
 	}
 
 	@Override
