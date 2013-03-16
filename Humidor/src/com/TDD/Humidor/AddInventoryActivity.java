8ee9394bf2f26c81a24831859101da14da662977
Megan Clark <megan_clark@student.uml.edu>
2013-03-15 17:49:15 -0700

    Adding missing files to commit

---
+++ Humidor/src/com/TDD/Humidor/AddInventoryActivity.java
@@ -0,0 +1,17 @@
+package com.TDD.Humidor;
+
+/*
+ * Author: Megan Clark
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddInventoryActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_inventory_activity);
+	}
+}
