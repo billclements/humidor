da36b669ebe3cb15d46739757f6332f46a6d2322
Megan Clark <megan_clark@student.uml.edu>
2013-03-20 15:49:47 -0700

    Added to my tests

--- TestHumidor/src/com/TDD/Humidor/test/TestMainActivity.java
+++ TestHumidor/src/com/TDD/Humidor/test/TestMainActivity.java
@@ -1,61 +1,61 @@
 package com.TDD.Humidor.test;
 
-/**
- * Author: Megan Clark
- */
-import android.app.Activity;
 import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
-import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
-import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityUnitTestCase;
 import android.test.TouchUtils;
+import android.test.UiThreadTest;
 import android.view.View;
 import android.widget.ListView;
 
 import com.TDD.Humidor.MainActivity;
 
 public class TestMainActivity extends
-		ActivityInstrumentationTestCase2<MainActivity> {
-	private Activity mActivity;
-	private Instrumentation mInstrumentation;
+		ActivityUnitTestCase<MainActivity> {
+
+	private Intent mStartIntent;
 	private ListView mListView;
+	private Instrumentation mInstrumentation;
+	private MainActivity mActivity;
+	private Context mContext;
 
 	public TestMainActivity() {
-		this("TestMainActivity");
-	}
-
-	public TestMainActivity(String name) {
 		super(MainActivity.class);
-		setName(name);
 	}
 
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		// This must be called before getActivity
-		// disabling touch mode allows sending key events
-		setActivityInitialTouchMode(false);
-		mActivity = getActivity();
+		mStartIntent = new Intent(Intent.ACTION_MAIN);
 		mInstrumentation = getInstrumentation();
-		mListView = getActivity().getListView();
+		mActivity = startActivity(mStartIntent, null, null);
+		mListView = (ListView) mActivity.findViewById(android.R.id.list);
+		mContext = getInstrumentation().getTargetContext();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 
 	/*
-	 * Test all preconditions and ensure that the fixture has been created
-	 * correctly
+	 * ====================================================================
+	 * TESTS
 	 */
-	public final void testPreconditions() {
+	public void testPreconditions() {
 		assertNotNull(mActivity);
 		assertNotNull(mListView);
 	}
 
-	// An array of 2 items should populate the ListView
-	public final void testListPopulatedCorrectly() {
-		assertNotNull(((ListActivity) mActivity).getListView().getChildCount());
+	// Is the list view populated?
+	public final void testListIsPopulated() {
+		assertNotNull(mListView.getChildCount());
 	}
 
-	// test onClick
-	public final void testListClick() {
+	// is the new activity launched on click?
+	public void testSubLaunch() {
 		int i;
 		int count = mListView.getChildCount();
 		mInstrumentation.waitForIdleSync();
@@ -63,22 +63,33 @@ public class TestMainActivity extends
 		ActivityMonitor monitor = mInstrumentation.addMonitor(intentFilter,
 				null, true);
 		assertEquals(0, monitor.getHits());
-		// click each entry in the list and check that it receives the click
+		// click each entry in the list and check that it receives the click, a
+		// new activity is started, and this activity shuts down properly
 		for (i = 0; i < count; i++) {
 			View child = mListView.getChildAt(i);
 			TouchUtils.clickView(this, child);
 			monitor.waitForActivityWithTimeout(3000);
 			assertEquals(i + 1, monitor.getHits());
+			assertNotNull(getStartedActivityIntent());
+			assertTrue(isFinishCalled());
 		}
 		mInstrumentation.removeMonitor(monitor);
-
 	}
-	
-	//TODO test on long press
-	
-	//TODO test content resolver working correctly
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	// can the selection be set to specific position?
+	@UiThreadTest
+	public void testSetSelection() {
+		int childCount = mListView.getChildCount();
+		for (int i = 0; i < childCount; i++) {
+			mListView.setSelection(i);
+			assertEquals("Set selection", i,
+					mListView.getSelectedItemPosition());
+		}
 	}
+
+	// TODO find test contextmenu
+	// TODO find test contentresolver
+	// TODO find test progressbar
+	// TODO find test lifecycle
+
 }
